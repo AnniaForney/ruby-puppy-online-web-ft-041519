5b85034a53e9e1fa4ec22ce82a14bc91f91764d1
Annia Forney <anniaforney@gmail.com>
2019-04-28 13:07:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,3 @@
 class Dog 
-  
-  attr_accessor :name, :dog 
-  
-  @@all = []
-  @@clear_all = 0 
-  
-  
-  def initialize (name, dog)
-    @name = name 
-    @@all= all 
-    @@clear_all = clear_all
-    @@count += 1 
-    @@genres << genre 
-    @@artists << artist
 end 
-  
-  
-  def self.clear_all 
-    @clear_all
-  end 
-  
-  def self.all
-  end 
-  
-  def self.new 
+
